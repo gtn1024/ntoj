1e153b7266b03702d48f7b89df783c17f18f2efc
Taoning Ge <gtn1024o@outlook.com>
2023-07-26 19:15:37 +0800

    取消全限定类名

--- server/src/main/kotlin/zip/ntoj/server/config/AppConfiguration.kt
+++ server/src/main/kotlin/zip/ntoj/server/config/AppConfiguration.kt
@@ -7,14 +7,14 @@ import zip.ntoj.server.service.FileService
 import zip.ntoj.server.service.FileUploadService
 
 @Configuration
-@EnableConfigurationProperties(zip.ntoj.server.config.InformationConfig::class, zip.ntoj.server.config.FileConfig::class)
+@EnableConfigurationProperties(InformationConfig::class, FileConfig::class)
 class AppConfiguration(
-    val fileConfig: zip.ntoj.server.config.FileConfig,
+    val fileConfig: FileConfig,
     val fileUploadService: FileUploadService,
 ) {
     @Bean
     fun fileService(): FileService {
-        return fileConfig.serviceImpl.getDeclaredConstructor(zip.ntoj.server.config.FileConfig::class.java, FileUploadService::class.java)
+        return fileConfig.serviceImpl.getDeclaredConstructor(FileConfig::class.java, FileUploadService::class.java)
             .newInstance(fileConfig, fileUploadService) as FileService
     }
 }
