e3beda0b6ef5fe5a8db1ee4e4eb6e23e602c6ed0
Taoning Ge <gtn1024o@outlook.com>
2023-08-28 15:15:44 +0800

    完善竞赛列表页面

--- server/src/main/kotlin/zip/ntoj/server/controller/ContestController.kt
+++ server/src/main/kotlin/zip/ntoj/server/controller/ContestController.kt
@@ -1,5 +1,6 @@
 package zip.ntoj.server.controller
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,6 +11,7 @@ import zip.ntoj.server.model.Contest
 import zip.ntoj.server.model.L
 import zip.ntoj.server.service.ContestService
 import zip.ntoj.shared.model.R
+import java.time.Instant
 
 @RestController
 @RequestMapping("/contest")
@@ -38,12 +40,22 @@ class ContestController(
         val id: Long,
         val title: String,
         val description: String?,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8") val startTime: Instant,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8") val endTime: Instant,
+        val type: Contest.ContestType,
+        val permission: Contest.ContestPermission,
+        val userCount: Int,
     ) {
         companion object {
             fun from(contest: Contest) = ContestDto(
                 id = contest.contestId!!,
                 title = contest.title,
                 description = contest.description,
+                startTime = contest.startTime,
+                endTime = contest.endTime,
+                type = contest.type,
+                permission = contest.permission,
+                userCount = contest.users.size,
             )
         }
     }
