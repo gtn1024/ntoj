25708bfa57492596e6c5c5c0d6614739d3be8c92
Taoning Ge <gtn1024o@outlook.com>
2023-09-02 15:52:22 +0800

    优化ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,13 @@
-import {defineConfig, PluginOption, splitVendorChunkPlugin} from 'vite'
+import type { PluginOption } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import visualizer from "rollup-plugin-visualizer";
-import UnoCSS from 'unocss/vite';
+import visualizer from 'rollup-plugin-visualizer'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command}) => ({
+export default defineConfig(({ command }) => ({
   define: {
-    isDev: command === 'serve'
+    isDev: command === 'serve',
   },
   plugins: [
     UnoCSS(),
@@ -23,7 +24,7 @@ export default defineConfig(({command}) => ({
       '/api': {
         target: 'http://localhost:18080',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: path => path.replace(/^\/api/, ''),
       },
       '/upload': {
         target: 'http://localhost:18080',
