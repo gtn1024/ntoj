6e2520f3f2962ac19c411d09f3fddaceab2ebfc2
Taoning Ge <gtn1024o@outlook.com>
2023-09-07 15:09:26 +0800

    优化移动端布局

--- web/src/pages/contest/ContestHome.tsx
+++ web/src/pages/contest/ContestHome.tsx
@@ -8,6 +8,7 @@ import type { HttpResponse } from '../../lib/Http.tsx'
 import { http } from '../../lib/Http.tsx'
 import { MarkdownArticle } from '../../components/MarkdownArticle.tsx'
 import { timeDiff, timeDiffString } from '../../lib/misc.ts'
+import { useLayout } from '../../hooks/useLayout.ts'
 
 export const ContestHome: React.FC = () => {
   const { id } = useParams()
@@ -21,16 +22,17 @@ export const ContestHome: React.FC = () => {
         throw err
       })
   })
+  const { isMobile } = useLayout()
   return (
-    <div p-2 flex items-start>
-      <div className={c('bg-white rounded-lg shadow-md pb-8 my-1', 'mx-2 w-2/3')}>
+    <div p-2 flex items-start max-w="[1200px]" m-auto className={c(isMobile && 'flex-col')}>
+      <div className={c('bg-white rounded-lg shadow-md pb-8 my-1', isMobile ? 'w-11/12 mx-auto' : 'mx-2 w-3/4')}>
         <div flex flex-col px-4 gap-4>
           <div pt-8>
             <MarkdownArticle data={contest?.description ?? ''}/>
           </div>
         </div>
       </div>
-      <div className={c('bg-white rounded-lg shadow-md pb-8 my-1', 'mx-2 w-1/3')}>
+      <div className={c('bg-white rounded-lg shadow-md pb-8 my-1', isMobile ? 'w-11/12 mx-auto' : 'mx-2 w-1/4')}>
         <div pt-8 px-4>
           <h2 font-light>{contest?.title}</h2>
         </div>
