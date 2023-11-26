4f79384fdb268b2de8b20810c81a0ae792dab696
Taoning Ge <gtn1024o@outlook.com>
2023-11-26 15:04:50 +0800

    chore: remove split vendor

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,5 @@
 import type { PluginOption } from 'vite'
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import visualizer from 'rollup-plugin-visualizer'
 import UnoCSS from 'unocss/vite'
@@ -12,7 +12,6 @@ export default defineConfig(({ command }) => ({
   plugins: [
     UnoCSS(),
     react(),
-    splitVendorChunkPlugin(),
     visualizer() as PluginOption,
   ],
   server: {
