991dc7ba3eec38534be49f66ff021ccdc4abd18d
Taoning Ge <gtn1024o@outlook.com>
2024-04-13 22:57:55 +0800

    chore(eslint): remove commented out rules in config

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -25,11 +25,6 @@ export default antfu(
       'react/no-unknown-property': 'off',
       'ts/no-unused-vars': 'warn',
       'ts/no-explicit-any': 'warn',
-      // '@typescript-eslint/no-unsafe-assignment': 'warn',
-      // '@typescript-eslint/no-unsafe-member-access': 'warn',
-      // '@typescript-eslint/no-unsafe-call': 'warn',
-      // '@typescript-eslint/no-unsafe-argument': 'warn',
-      // '@typescript-eslint/no-explicit-any': 'warn',
     },
   }),
 )
