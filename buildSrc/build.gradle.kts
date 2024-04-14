174ebf625e3fbb0802bea41937ba8b3f9857fedd
Taoning Ge <gtn1024o@outlook.com>
2024-04-14 20:30:08 +0800

    chore: update Kotlin to 1.9.23

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 
 dependencies {
     implementation(gradleApi())
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
 }
 
