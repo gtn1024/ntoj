75607ae449c8798d0d3313d109e83cd242f81edc
Xulong Yan <3528613068@qq.com>
2024-04-17 15:27:31 +0800

    feat: add create article controller

---
+++ server/src/main/kotlin/com/github/ntoj/app/server/controller/ArticleController.kt
@@ -0,0 +1,81 @@
+package com.github.ntoj.app.server.controller
+
+import cn.dev33.satoken.annotation.SaCheckLogin
+import cn.dev33.satoken.stp.StpUtil
+import com.github.ntoj.app.server.ext.success
+import com.github.ntoj.app.server.model.Article
+import com.github.ntoj.app.server.model.User
+import com.github.ntoj.app.server.service.ArticleService
+import com.github.ntoj.app.server.service.UserService
+import com.github.ntoj.app.shared.model.R
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+@RestController
+@RequestMapping("/article")
+class ArticleController(
+    val articlesService: ArticleService,
+    val userService: UserService,
+) {
+    @SaCheckLogin
+    @PostMapping
+    fun create(
+        @RequestBody articleRequest: ArticleRequest,
+    ): ResponseEntity<R<ArticleDto>> {
+        require(!articleRequest.title.isNullOrBlank()) { "文章标题不能为空" }
+        require(!articleRequest.content.isNullOrBlank()) { "文章内容不能为空" }
+
+        val user = userService.getUserById(StpUtil.getLoginIdAsLong())
+
+        var article =
+            Article(
+                title = articleRequest.title,
+                content = articleRequest.content,
+                author = user,
+            )
+
+        article = articlesService.create(article)
+        return R.success(200, "创建成功", ArticleDto.from(article))
+    }
+}
+
+data class ArticleRequest(
+    val title: String?,
+    val content: String?,
+)
+
+data class ArticleDto(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val author: UserDto,
+    val createdAt: Instant,
+) {
+    data class UserDto(
+        val username: String,
+        val realName: String?,
+    ) {
+        companion object {
+            fun from(user: User): UserDto =
+                UserDto(
+                    username = user.username,
+                    realName = user.realName,
+                )
+        }
+    }
+
+    companion object {
+        fun from(article: Article) =
+            ArticleDto(
+                id = article.articleId!!,
+                title = article.title,
+                content = article.content,
+                author = UserDto.from(article.author),
+                createdAt = article.createdAt!!,
+            )
+    }
+}
--- server/src/main/kotlin/com/github/ntoj/app/server/service/ArticleService.kt
+++ server/src/main/kotlin/com/github/ntoj/app/server/service/ArticleService.kt
@@ -1,11 +1,18 @@
 package com.github.ntoj.app.server.service
 
+import com.github.ntoj.app.server.model.Article
 import com.github.ntoj.app.server.repository.ArticleRepository
 import org.springframework.stereotype.Service
 
-interface ArticleService
+interface ArticleService {
+    fun create(article: Article): Article
+}
 
 @Service
 class ArticleServiceImpl(
     val articleRepository: ArticleRepository,
-) : ArticleService
+) : ArticleService {
+    override fun create(article: Article): Article {
+        return articleRepository.save(article)
+    }
+}
