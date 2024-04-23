19b40062520dac821e01b0db170e1152580ef34e
Taoning Ge <gtn1024o@outlook.com>
2024-04-23 11:26:37 +0800

    chore: reconfigure eslint

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,30 +1,23 @@
 import antfu from '@antfu/eslint-config'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const compat = new FlatCompat()
 
 export default antfu(
   {
+    // Enable stylistic formatting rules
+    stylistic: true,
+
     unocss: true,
+
+    typescript: true,
     react: true,
-    ignores: [
-      '**/*.json',
-    ],
+
+    // Disable jsonc and yaml support
+    jsonc: false,
+    yaml: false,
   },
-  ...compat.config({
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react-hooks/recommended',
-    ],
+  {
     rules: {
-      'react-refresh/only-export-components': 'off',
       'curly': 'off',
       'style/brace-style': 'off',
-      '@typescript-eslint/brace-style': 'off',
-      'max-statements-per-line': 'off',
-      'react/no-unknown-property': 'off',
-      'ts/no-unused-vars': 'warn',
-      'ts/no-explicit-any': 'warn',
     },
-  }),
+  },
 )
