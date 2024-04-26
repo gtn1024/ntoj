42d52011c5df5ae2f830ff7f56d71ad3c0ddd165
Taoning Ge <gtn1024o@outlook.com>
2024-04-26 23:38:43 +0800

    feat: add admin homework create and update endpoints

--- server/src/main/kotlin/com/github/ntoj/app/server/controller/admin/AdminHomeworkController.kt
+++ server/src/main/kotlin/com/github/ntoj/app/server/controller/admin/AdminHomeworkController.kt
@@ -6,15 +6,22 @@ import cn.dev33.satoken.annotation.SaMode
 import com.github.ntoj.app.server.ext.success
 import com.github.ntoj.app.server.model.L
 import com.github.ntoj.app.server.model.dtos.admin.HomeworkDto
+import com.github.ntoj.app.server.model.entities.Homework
+import com.github.ntoj.app.server.service.GroupService
 import com.github.ntoj.app.server.service.HomeworkService
+import com.github.ntoj.app.server.service.ProblemService
 import com.github.ntoj.app.shared.model.R
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
 
 @RestController
 @RequestMapping("/admin/homework")
@@ -22,6 +29,8 @@ import org.springframework.web.bind.annotation.RestController
 @SaCheckRole(value = ["COACH", "ADMIN", "SUPER_ADMIN"], mode = SaMode.OR)
 class AdminHomeworkController(
     val homeworkService: HomeworkService,
+    val problemService: ProblemService,
+    val groupService: GroupService,
 ) {
     @GetMapping("{id}")
     fun get(
@@ -53,6 +62,47 @@ class AdminHomeworkController(
         )
     }
 
+    @PostMapping
+    fun create(
+        @RequestBody homeworkRequest: HomeworkRequest,
+    ): ResponseEntity<R<HomeworkDto>> {
+        var homework =
+            Homework(
+                homeworkRequest.title,
+                Instant.ofEpochSecond(homeworkRequest.startTime),
+                Instant.ofEpochSecond(homeworkRequest.endTime),
+                homeworkRequest.groups.map {
+                    groupService.get(it)
+                },
+                homeworkRequest.problems.map {
+                    problemService.get(it)
+                },
+            )
+        homework = homeworkService.create(homework)
+        return R.success(200, "创建成功", HomeworkDto.from(homework))
+    }
+
+    @PatchMapping("{id}")
+    fun update(
+        @PathVariable id: Long,
+        @RequestBody homeworkRequest: HomeworkRequest,
+    ): ResponseEntity<R<HomeworkDto>> {
+        var homework = homeworkService.get(id)
+        homework.title = homeworkRequest.title
+        homework.startTime = Instant.ofEpochSecond(homeworkRequest.startTime)
+        homework.endTime = Instant.ofEpochSecond(homeworkRequest.endTime)
+        homework.groups =
+            homeworkRequest.groups.map {
+                groupService.get(it)
+            }
+        homework.problems =
+            homeworkRequest.problems.map {
+                problemService.get(it)
+            }
+        homework = homeworkService.update(homework)
+        return R.success(200, "修改成功", HomeworkDto.from(homework))
+    }
+
     @DeleteMapping("/{id}")
     fun remove(
         @PathVariable id: Long,
@@ -60,4 +110,12 @@ class AdminHomeworkController(
         homeworkService.remove(id)
         return R.success(200, "删除成功")
     }
+
+    data class HomeworkRequest(
+        val title: String,
+        val startTime: Long,
+        val endTime: Long,
+        val groups: List<Long>,
+        val problems: List<Long>,
+    )
 }
