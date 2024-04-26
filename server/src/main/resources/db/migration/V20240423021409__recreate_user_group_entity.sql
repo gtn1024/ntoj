aae87cfde8dc8fb8bcc9997cce94b460325c253c
Taoning Ge <gtn1024o@outlook.com>
2024-04-26 21:54:24 +0800

    fix: fix migrate script

--- server/src/main/resources/db/migration/V20240423021409__recreate_user_group_entity.sql
+++ server/src/main/resources/db/migration/V20240423021409__recreate_user_group_entity.sql
@@ -10,18 +10,18 @@ ALTER TABLE t_groups_users
 ALTER TABLE t_groups_users
     ADD user_id BIGINT;
 
-ALTER TABLE t_groups_users
-    ALTER COLUMN group_id SET NOT NULL;
-
-ALTER TABLE t_groups_users
-    ALTER COLUMN user_id SET NOT NULL;
-
 -- Copy data from old columns to new columns
 UPDATE t_groups_users
     SET group_id = t_groups_group_id;
 
 UPDATE t_groups_users
     SET user_id = users_user_id;
+
+ALTER TABLE t_groups_users
+    ALTER COLUMN group_id SET NOT NULL;
+
+ALTER TABLE t_groups_users
+    ALTER COLUMN user_id SET NOT NULL;
 
 ALTER TABLE t_groups_users
     ADD CONSTRAINT fk_tgrouse_on_group FOREIGN KEY (group_id) REFERENCES t_groups (group_id);
