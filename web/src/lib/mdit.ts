1cee92f90cde478556db58e0e4167a0bc11ee9cd
Taoning Ge <i@gtn1024.me>
2024-05-05 14:15:04 +0800

    refactor: pack markdown-it

--- web/src/lib/mdit.ts
+++ web/src/lib/mdit.ts
@@ -3,14 +3,30 @@ import { imgLazyload } from '@mdit/plugin-img-lazyload'
 import mathjax3 from 'markdown-it-mathjax3'
 import Shiki from '@shikijs/markdown-it'
 
-const mdit = MarkdownIt()
-mdit.use(await Shiki({
-  themes: {
-    light: 'vitesse-light',
-    dark: 'vitesse-dark',
-  },
-}))
-mdit.use(imgLazyload)
-mdit.use(mathjax3)
+class Mdit {
+  mdit: MarkdownIt
+
+  constructor() {
+    this.mdit = MarkdownIt()
+    this.init()
+  }
+
+  private async init() {
+    this.mdit.use(await Shiki({
+      themes: {
+        light: 'vitesse-light',
+        dark: 'vitesse-dark',
+      },
+    }))
+    this.mdit.use(imgLazyload)
+    this.mdit.use(mathjax3)
+  }
+
+  render(content: string): string {
+    return this.mdit.render(content)
+  }
+}
+
+const mdit = new Mdit()
 
 export { mdit }
